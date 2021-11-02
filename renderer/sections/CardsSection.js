9c16889064f18e157e11edf37d0c782535d6d9e8
Raul C. Rivero <rahulrcr98@gmail.com>
2021-11-02 18:18:08 -0400

    add donations

--- renderer/sections/CardsSection.js
+++ renderer/sections/CardsSection.js
@@ -12,10 +12,13 @@ export default function CardsSection() {
   }, [])
 
   return (
-    <div className="w-full flex flex-wrap justify-center mt-10">
-      <ConnectionCard linkZoneController={linkZone} />
-      {/* <NetworkCard linkZoneController={linkZone} /> */}
-      <UssdCard linkZoneController={linkZone} />
+    <div>
+      <div className="w-full flex flex-wrap justify-center mt-10">
+        <ConnectionCard linkZoneController={linkZone} />
+        {/* <NetworkCard linkZoneController={linkZone} /> */}
+        <UssdCard linkZoneController={linkZone} />
+      </div>
+      <h2 className="mt-2 text-center w-full text-gray-800">Considera hacernos una donación mediante 💰 <a onClick={() => require('electron').shell.openExternal("https://qvapay.com/payme/linkzone")} className="cursor-pointer font-bold underline text-blue-600">QvaPay</a></h2>
     </div>
   )
 }
