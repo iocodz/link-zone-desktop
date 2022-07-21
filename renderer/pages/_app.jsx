13c43ff9efb07d29123fd505b44ef9c296f44958
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-20 22:38:59 -0400

    add modal component and refactor

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
+import Header from '../components/Header';
 import Loading from "../components/Loading";
 
 import '../styles/globals.css';
--- renderer/sections/CardsSection.js
+++ renderer/sections/CardsSection.js
@@ -1,12 +1,8 @@
 import React, {useEffect, useState} from "react";
 import ConnectionCard from "../components/ConnectionCard";
-import NetworkCard from "../components/NetworkCard";
 import UssdCard from "../components/UssdCard";
-import LinkZone from "../types/LinkZone";
 
-export default function CardsSection() {
-
-  const linkZone = new LinkZone()
+export default function CardsSection({linkZone}) {
 
   useEffect(() => {
   }, [])
@@ -15,7 +11,6 @@ export default function CardsSection() {
     <div>
       <div className="w-full flex flex-wrap justify-center mt-10">
         <ConnectionCard linkZoneController={linkZone} />
-        <NetworkCard linkZoneController={linkZone} />
         <UssdCard linkZoneController={linkZone} />
       </div>
       <h2 className="mt-2 text-center w-full text-gray-800">Considera hacer una <a onClick={() => require('electron').shell.openExternal("https://qvapay.com/payme/linkzone")} className="cursor-pointer font-bold underline text-blue-600">donación 💰</a> </h2>
