35bd49e615cb0aa39a37e8db140a6f43b7a05109
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-20 23:00:45 -0400

    add about and donations section

--- renderer/components/Header.js
+++ renderer/components/Header.js
@@ -1,10 +1,11 @@
+import AboutAndDonations from "./Settings/AboutAndDonations";
 import NetworkSettings from "./Settings/NetworkSettings";
 
 export default function Header({ linkZoneController }) {
   return (
       <>
         <NetworkSettings linkZoneController={linkZoneController} />
-        {/* <AboutAndDonations /> */}
+        <AboutAndDonations />
       </>
     )
 }
