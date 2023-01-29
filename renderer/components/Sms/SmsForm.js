82ae1dbd7d1bbc91e58eb88e8d3ca74e37f2b994
Raul C. Rivero <rahulrcr98@gmail.com>
2023-01-29 13:37:05 -0500

    feat: sending sms status

--- renderer/components/Sms/SmsForm.js
+++ renderer/components/Sms/SmsForm.js
@@ -2,14 +2,20 @@ import { useState } from "react";
 import { useContact } from "../../hooks/useContact";
 import { useSms } from "../../hooks/useSms";
 import LinkZone from "../../LinkZone";
+import SendIcon from "../UI/SendIcon";
+import Spinner from "../UI/Spinner";
 
 export default function SmsForm() {
   const [contact, changeContact] = useSms();
   const [contacts] = useContact();
   const [text, setText] = useState("");
+  const [sending, setSending] = useState(false);
 
-  const handleSubmitSms = async () => {
+  const handleSubmitSms = async (e) => {
+    e.preventDefault();
     if(!text) return;
+
+    setSending(true);
     
     const linkZone = new LinkZone();
     const phone = contacts.SmsList.find(
@@ -17,13 +23,15 @@ export default function SmsForm() {
     ).PhoneNumber;
     
     await linkZone.sendSms(text, phone);
+
+    setSending(false);
     
     changeContact(contact);
     setText("");
   };
 
   return (
-    <form className="flex items-center justify-between w-full p-3 border-t border-gray-300">
+    <form onSubmit={handleSubmitSms} className="flex items-center justify-between w-full p-3 border-t border-gray-300">
       <input
         onChange={(e) => setText(e.target.value)}
         type="text"
@@ -32,15 +40,8 @@ export default function SmsForm() {
         name="message"
         required
       />
-      <button type="submit" onClick={handleSubmitSms}>
-        <svg
-          className="w-5 h-5 text-gray-500 origin-center transform rotate-90"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-        </svg>
+      <button type="submit">
+        {sending ? <Spinner /> : <SendIcon />}
       </button>
     </form>
   );
