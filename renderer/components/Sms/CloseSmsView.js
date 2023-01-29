bddcdd5258935b8b81cec3ce069d88a9c78f6c00
Raul C. Rivero <rahulrcr98@gmail.com>
2023-01-29 13:39:12 -0500

    feat: close icon

--- renderer/components/Sms/CloseSmsView.js
+++ renderer/components/Sms/CloseSmsView.js
@@ -1,25 +1,14 @@
 import React, { useRouter } from "next/router";
+import CloseIcon from "../UI/CloseIcon";
 
 export default function CloseSmsView() {
   const router = useRouter();
   return (
     <div className="relative flex justify-between items-center p-3 border-b border-gray-300 text-gray-800">
       <div></div>
-      <svg
-        onClick={() => router.push("/home")}
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 cursor-pointer rounded-full hover:bg-gray-200"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
+      <button onClick={() => router.push("/home")}>
+        <CloseIcon />
+      </button>
     </div>
   );
 }
